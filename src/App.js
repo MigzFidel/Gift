364969f26e6847b92e5cbe01a78eaf3ab1f1a368
Migz Fidel <miguel@flint.com.ph>
2022-08-19 05:33:42 +0800

    gift

--- src/App.js
+++ src/App.js
@@ -1,24 +1,227 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
+import LoadingIcon from './assets/Loading.gif';
+import MenuIcon from './assets/Menu.gif';
+import Swal from 'sweetalert2';
+import AngryIcon from './assets/angry.gif'
+import CryingIcon from './assets/crying.gif';
+import LonelyIcon from './assets/lonely.gif';
+import SadIcon from './assets/sad.gif';
+import SuperAngry from './assets/SuperAngry.gif';
+import SendGift from './assets/sendgift.gif';
+import CelebrateIcon from './assets/Celebrate.gif'
+import Letter from './Letter';
+
 
 function App() {
+  const [submit,setSubmit] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [denied, setDenied] = useState(false);
+  const [msg, setMes] = useState('');
+  const [final, setFinal] = useState(false);
+
+  if(denied){
+    Swal.fire({
+      title: 'Are you sure? 😢',
+      allowOutsideClick: false,
+      showDenyButton: true,
+      imageUrl: SadIcon,
+      imageWidth: 200,
+      imageHeight: 200,
+      imageAlt: 'Crying',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Gusto',
+      denyButtonText: 'Ayaw'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setSubmit(true)
+        setDenied(false)
+      }
+      if (result.isDenied) {
+        Swal.fire({
+          title: 'Sure ka ba talaga ayaw mo? 🥺',
+          allowOutsideClick: false,
+          showDenyButton: true,
+          imageUrl: CryingIcon,
+          imageWidth: 200,
+          imageHeight: 200,
+          imageAlt: 'Crying',
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes gusto',
+          denyButtonText: 'No ayaw'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            setSubmit(true)
+            setDenied(false)
+          }
+          if(result.isDenied){
+            Swal.fire({
+              title: 'Hindi ka ba naawa sakin? 😭',
+              showDenyButton: true,
+              allowOutsideClick: false,
+              imageUrl: LonelyIcon,
+              imageWidth: 200,
+              imageHeight: 200,
+              imageAlt: 'Lonely',
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Yes gustong gusto',
+              denyButtonText: 'No ayoko'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                setSubmit(true)
+                setDenied(false)
+              }
+              if (result.isDenied) {
+                Swal.fire({
+                  title: 'Ang Kulet mo! Yes mo na! 😠',
+                  imageUrl: AngryIcon,
+                  imageWidth: 200,
+                  imageHeight: 200,
+                  imageAlt: 'Angry',
+                  showDenyButton: true,
+                  allowOutsideClick: false,
+                  confirmButtonColor: '#3085d6',
+                  confirmButtonText: 'YEESS!!!',
+                  denyButtonText: 'No ayoko nga'
+                }).then((result) => {
+                  if (result.isDenied){
+                    Swal.fire({
+                      title: 'Wala ka na choice kundi YESSSS!!!!!!!😡',
+                      imageUrl: SuperAngry,
+                      imageWidth: 200,
+                      imageHeight: 200,
+                      imageAlt: 'Angry',
+                      showDenyButton: false,
+                      allowOutsideClick: false,
+                      confirmButtonColor: '#3085d6',
+                      confirmButtonText: 'YESSSS!!!!!!!',
+                    }).then((result)=>{
+                      if (result.isConfirmed) {
+                        setSubmit(true)
+                        setDenied(false)
+                      }
+                    })
+                  }
+                })
+              }
+            })
+          }
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    if(submit){
+      setTimeout(() => {
+        setFinal(true)
+      }, 5000);
+    }
+  }, [submit])
+
+  useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000);
+  },[])
+
+  const string = 'Will you accept my gift?😊'
+  const index = useRef(-1);
+
+  useEffect(() => {
+    if(!loading){
+      function tick() {
+        setMes(prev => prev + string[index.current]);
+        index.current++;
+      }
+      if (index.current < string.length-1) {
+        let addChar = setInterval(tick, 100);
+        return () => clearInterval(addChar);
+      }
+    }
+  }, [msg,loading]);
+
+  const Menu = () =>{
+    return(
+      <div>
+        <div className="loading-icon text-center">
+          <img src={MenuIcon} width="150rem" height="150rem" alt="Menu"/>
+        </div>
+        <div className="message-text text-center">
+          <h3>Chrissa!!!</h3>
+          <h3>{msg}</h3>
+        </div>
+        {index.current === 25 ? 
+          <div className="text-center">
+            <button className="btn btn-primary m-2" onClick={()=>{setSubmit(true)}}>YES 🥰</button>
+            <button className="btn btn-danger" onClick={()=>{setDenied(true)}}>NO 😔</button>
+          </div>
+          :
+          null
+        }    
+      </div>
+    );
+  }
+
+
+
+  const Submitted = () =>{
+    if(!final){
+      return(
+        <div>
+          <div className="loading-icon text-center">
+            <img src={SendGift} width="200rem" height="200rem" alt="Loading"/>
+          </div>
+          <div className="loading-text text-center">
+            <h3>Sending Gift...</h3>
+          </div>
+        </div>
+      );
+    }
+    else{
+      return(
+        <div>
+          <div className="loading-icon text-center">
+          <img src={CelebrateIcon} width="175rem" height="150rem" alt="Loading"/>
+          </div>
+          <div className="loading-text text-center">
+            <h4>Yehey!!! Wear mo yung ring</h4>
+            <p style={{fontSize: 15}}>Papicturan naman sa kamay mo 🥺</p>
+          </div>
+          {/* <div className="loading-text text-center">
+            <button className="btn btn-success">📝Read Letter📝</button>
+          </div> */}
+            <Letter/>
+        </div>
+      );
+    }
+
+  }
+
+
+  const Loading = () =>{
+    
+    return(
+      <div>
+        <div className="loading-icon text-center">
+          <img src={LoadingIcon} width="200rem" height="200rem" alt="Loading"/>
+        </div>
+        <div className="loading-text text-center">
+          <h3>Please Wait...</h3>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="text_box">
+      {loading ? <Loading/> : <>{submit? <Submitted/> : <Menu/>}</>}
     </div>
+  </div>
   );
 }
 
---
+++ src/Letter.js
@@ -0,0 +1,74 @@
+
+
+function Letter(){
+
+
+return (<div>
+  <div className="text-center">
+  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  📝Read Letter📝
+  </button>
+  </div>
+  
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header" style={{backgroundColor: "pink"}}>
+          <h5 class="modal-title" id="exampleModalLabel" style={{color: "black"}}>Letter</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body" style={{backgroundColor: "rgb(255, 230, 240)"}}>
+          <p style={{fontSize: 12, color: "black"}}>
+          Okay lang wag mo masyado to basahin.<br/>
+          I just wanted to make a letter and express my feelings for you.<br/>
+          Kahit kilala kita for a short period of time pero feeling ko matagal na kilala.<br/>
+          Marami ka napafeel sakin that i never experience before. Sa dami mo kwento.<br/>
+          Ang dami ko natutunan about life, relationships, sex, friends, family at iba pa.<br/>
+          Sorry wala ako minsan mashare. Sobra naging focus lang ako sa studies and my life is somewhat boring din.<br/>
+          Just playing games and study lang ako tapos hindi pa ako mapabarkada nun kesa ngayon.<br/>
+          Lumayo talaga ako sa mga tao dati kaya ganun AHHAHA.<br/>
+          Yung stories mo talaga sobra interesting. Ang sarap talaga makinig.<br/>
+          <br/>
+          Sa galing mo rin sa music. Sobra na urge ako magmusic ulit despite konti lang alam ko sa gitara.<br/>
+          Mas gusto ko ngayon magpiano. Actually gusto ko talaga magpiano pero wala ako motivation. <br/>
+          Ikaw talaga nagmotivate sakin na magpiano.<br/>
+          Sobra hanga talaga ako sa galing mo AHHAHA. Mala mozart ka ata HAHAHA.<br/> 
+          Ang dami mo instrument natutugtog at pinapakinggan mo lang para play mo yun. <br/>
+          Yung boses mo mala lea salonga HAHAHHA. Pero seryoso talaga maganda ng boses mo.<br/>
+          <br/>
+          Hindi ako nagpapasad boi or papakaawa.<br/>
+          Nafeel ko talaga dati pa sobra boring ko tao at sobra tahimik ko lang na tao.<br/>
+          Seryoso hindi talaga ako masalita at machat dati. As in sobra tahimik ko talaga. <br/>
+          Sobra bihira ako makipagusap.<br/>
+          Hindi ako pala share nangstories at masocmeds.<br/>
+          Hindi ko rin akalain nagkakaroon parin ako ng friends despite yung personality ko AHAHAHHA.<br/>
+          <br/>
+          You’re really the funniest, prettiest, smartest and talented girl i ever met.<br/>
+          Kahit maging acquintance, friends, bestfriends, partner ,couple, spouse or kahit ano pa tayo.<br/>
+          You’re really really really special for me. I won’t let you go in my life. I love who you are wisha.<br/>
+          I’m really glad talaga i met you and I enjoy teaching and playing with you.<br/>
+          Best student kaya kita sa dami ko na naturuan about sa programming HAHAHAH.<br/>
+          Sana makita kita ulit sa personal balang araw.<br/>
+          <br/>
+          Thank you very much wisha. Sana magtuloy tuloy lang to.<br/>
+          I’ll always be there for you. From ups and downs mo.<br/>
+          Kahit inis na inis ka na sakin dahil sa mga games, ang slow ko, sa spelling ko, at minsan hindi ako makaintindi sa feelings mo.<br/>
+          Pagaling ka mabuti para makagala ka tapos hindi ka nahihiya.<br/>
+          Kung saan ka lumipat na school. Kung magIT ka or tuloy mo compsci kaya pa kita turaan.<br/>
+          Support kita at help kita sa mga pangarap mo at goals.<br/>
+          I hope you appreciate my simple program and small gift ko sayo. Enjoy!!!😊<br/>
+          <br/>
+          Tara laro Outlast 2 o Devour. Nakakamiss sigawan natin HAHAHA
+          </p>
+        </div>
+        <div class="modal-footer" style={{backgroundColor: "pink"}}>
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+);
+}
+  
+export default Letter;
